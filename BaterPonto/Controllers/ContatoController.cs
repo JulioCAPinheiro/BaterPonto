e9805afb642a7114fab518a5224399fea160e408
Julio Cesar <juliocesarap.df@gmail.com>
2022-10-15 14:25:57 -0300

    Adicionar arquivos de projeto.

---
+++ BaterPonto/Controllers/ContatoController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BaterPonto.Controllers
+{
+    public class ContatoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        public IActionResult Editar()
+        {
+            return View();
+        }
+
+        public IActionResult ApagarConfirmacao()
+        {
+            return View();
+        }
+    }
+}
